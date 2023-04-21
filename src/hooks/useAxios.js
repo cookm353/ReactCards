08cc7036039a6b7ac240c591bb96d4680bc92d3f
Matthew Cook <cookm353@gmail.com>
2023-04-21 16:58:50 -0400

    Built hook

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,7 +1,18 @@
 import { useState } from "react"
+import uuid from "uuid"
+import axios from "axios"
 
+/* Hook to make an AJAX request using Axios */
 const useAxios = () => {
+    const [ cards, setCards ] = useState([])
 
+    const addCard = async (url="https://deckofcardsapi.com/api/deck/new/draw/") => {        
+        console.log(url)
+        const response = await axios.get(url)
+        setCards(cards => [...cards, { ...response.data, id: uuid() }]);
+    }
+
+    return [ cards, addCard ]
 }
 
 export default useAxios
