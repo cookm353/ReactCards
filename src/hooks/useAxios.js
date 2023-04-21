bc8ebdb0696c6f125d38e3a67e4f37314095f930
Matthew Cook <cookm353@gmail.com>
2023-04-21 17:16:58 -0400

    Refactored to accept URL for requests

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -3,10 +3,10 @@ import uuid from "uuid"
 import axios from "axios"
 
 /* Hook to make an AJAX request using Axios */
-const useAxios = (url) => {
+const useAxios = () => {
     const [ cards, setCards ] = useState([])
 
-    const addCard = async () => {
+    const addCard = async (url) => {
         const response = await axios.get(url)
         setCards(cards => [...cards, { ...response.data, id: uuid() }]);
     }
